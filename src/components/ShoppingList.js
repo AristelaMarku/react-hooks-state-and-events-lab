301750a52575f0888eb14b7e9d8fe321bb7b0b37
Aristela Marku <aristela.marku@gmail.com>
2021-10-14 23:55:01 +0200

    commit tree f48b687f37f6629ba1f2edf163ff8feb511fa7cc

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,11 +1,27 @@
-import React from "react";
+import React, {useState} from "react";
+import { findDOMNode } from "react-dom";
 import Item from "./Item";
 
 function ShoppingList({ items }) {
+  //console.log(items)
+  const [selectedCategory, setSelectedCategory]=useState("Filter by category")
+  
+  function handelFilter(e){
+    setSelectedCategory(e.target.value)
+  }
+  const itemsToRender = items.filter((item)=>{
+    if(selectedCategory==="Filter by category"){
+      return true
+    }else {
+      return item.category===selectedCategory
+    }
+  })
+
+
   return (
     <div className="ShoppingList">
       <div className="Filter">
-        <select name="filter">
+        <select name="filter" onChange={handelFilter}>
           <option value="All">Filter by category</option>
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
@@ -13,7 +29,7 @@ function ShoppingList({ items }) {
         </select>
       </div>
       <ul className="Items">
-        {items.map((item) => (
+        {itemsToRender.map((item) => (
           <Item key={item.id} name={item.name} category={item.category} />
         ))}
       </ul>
