88021a015a7d06b64fd038163786d63e85a66d6b
Aristela Marku <aristela.marku@gmail.com>
2021-10-15 09:23:19 +0200

    Done.

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,16 @@
-import React from "react";
+import React,{useState} from "react";
 
 function Item({ name, category }) {
+  const [card,setToCard] = useState(true)
+   
+  function changeCardButton(){
+    setToCard((card)=>!card)
+  }
   return (
-    <li className="">
+    <li className={!card? "in-cart" : " "}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className="add" onClick={changeCardButton}>{card ?"Add to Cart" : "Remove From Cart"} </button>
     </li>
   );
 }
